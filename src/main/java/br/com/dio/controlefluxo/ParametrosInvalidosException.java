b029434d50f9c00a067115ad25685bc932eb0289
magnoclear <magnoapt@gmail.com>
2025-03-01 20:01:32 -0300

    Fluxo finalizado

---
+++ src/main/java/br/com/dio/controlefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package br.com.dio.controlefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
